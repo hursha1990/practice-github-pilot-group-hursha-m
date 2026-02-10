17e2548c67f03f925fe966d7aeb5cf2489e7acaf
Hursha Parveen <hurshaparveen@gmail.com>
2026-02-09 18:21:44 -0600

    initial commit

---
+++ src/main/java/com/example/practice_github_pilot_group_hursha_m/PracticeGithubPilotGroupHurshaMApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_github_pilot_group_hursha_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubPilotGroupHurshaMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubPilotGroupHurshaMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice_github_pilot_group_hursha_m/PracticeGithubPilotGroupHurshaMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_github_pilot_group_hursha_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubPilotGroupHurshaMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
